67ef9b98cb7d9cb866788bab4852e5f62544992d
geksafobiya <asja653@gmail.com>
2020-09-30 00:25:42 +0300

    Add class Car

--- src/CarList.java
+++ src/CarList.java
@@ -1,29 +1,30 @@
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 public class CarList {
-    public List <Car> getBrand(List<Car> cars, String brand) {
-        List<Car> result = new ArrayList();
-        for (Car car: cars) {
-            if (car.getBrand().equals(brand))
+    public List<Car> getBrandCar(List<Car> cars, String brand){
+        List<Car> result = new ArrayList<>();
+        for(Car car: cars)
+        {
+            if(car.getBrand().equals(brand))
                 result.add(car);
         }
         return result;
     }
-        public List <Car> getBrand(List<Car> cars, int n){
-            List<Car> result = new ArrayList();
-            for (Car car: cars) {
-                if (car.getAge() > n)
-                    result.add(car);
-            }
-            return result;
-        }
     public List<Car> getYearPrice(List<Car> cars, int year, int price)
     {
-        List<Car> result = new ArrayList();
+        List<Car> result = new ArrayList<>();
         for(Car car: cars)
         {
             if((car.getYear() == year) && (car.getCost() > price))
+                result.add(car);
+        }
+        return result;
+    }
+
+    public List <Car> getBrandAge(List<Car> cars, int n){
+        List<Car> result = new ArrayList();
+        for (Car car: cars) {
+            if (car.getAge() > n)
                 result.add(car);
         }
         return result;
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-public class task_para {
+
+public class Main {
     public static void main(String[] args)
     {
         Car car1 = new Car(12, "Merc", 1989, "yellow", 154, 12, "kuzov", 14);
-        Car car2 = new Car(12, "Merc", 1991, "blue", 124, 11, "kuzov11", 12);
-        List<Car> cars = Arrays.asList(car1, car2);
+        Car car2 = new Car(13, "Merc", 1991, "blue", 124, 11, "kuzov11", 12);
+        Car car3 = new Car(11, "BMW", 1991, "green", 124, 13, "kuzov12", 11);
+        Car car4 = new Car(14, "Toyota", 1991, "blue", 124, 11, "kuzov11", 10);
 
-        CarList car__ = new CarList();
+        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+
+        CarList carChosenBrand = new CarList();
         System.out.println("Список Машин марки мэрс:");
-        List<Car> resultList1 = car__.getBrandCar(cars, "Merc");
-        for(Car a: resultList1)
-            System.out.println(a.toString());
+        List<Car> resultList1 = carChosenBrand.getBrandCar(cars, "BMW");
+        System.out.println(resultList1.toString());
+        //for(Car a: resultList1)
+        //    System.out.println(a.toString());
     }
 
 }
+
